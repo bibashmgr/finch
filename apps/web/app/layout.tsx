ca7e8d2d376283a44799aba157afcd7160049819
bibashmgr <bibashthapamagar21@gmail.com>
2026-01-30 18:31:09 +0545

    refactor: make some changes in root page and layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import type { Metadata } from "next";
+// import localFont from 'next/font/local';
+import { Outfit, Courier_Prime, Open_Sans } from "next/font/google";
 
-import '@repo/ui/globals.css';
+import "@repo/ui/globals.css";
 
-import { ThemeProvider } from '@/components/provider/theme-provider';
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
+const fontSans = Open_Sans({
+  variable: "--font-sans",
 });
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
+const fontMono = Courier_Prime({
+  variable: "--font-mono",
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
-  title: 'Finch',
+  title: "Finch",
 };
 
 export default function RootLayout({
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${fontSans.variable} ${fontMono.variable}`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
