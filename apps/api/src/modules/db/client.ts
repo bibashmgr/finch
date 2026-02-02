1c6bc635828de927f150a67e4aa7634170052e19
bibashmgr <bibashthapamagar21@gmail.com>
2026-02-02 23:30:54 +0545

    feat(api): set database transaction by default

--- apps/api/src/modules/db/client.ts
+++ apps/api/src/modules/db/client.ts
@@ -1,16 +1,18 @@
-// import { TransactionalAdapterDrizzleOrm } from "@nestjs-cls/transactional-adapter-drizzle-orm";
-
 import "dotenv/config";
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { TransactionalAdapterDrizzleOrm } from "@nestjs-cls/transactional-adapter-drizzle-orm";
+
+import * as schema from "./schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
-const db = drizzle({ client: pool });
+
+const db = drizzle({ client: pool, schema });
 
 export type DB = typeof db;
 
-// export type DbTransactionAdapter = TransactionalAdapterDrizzleOrm<typeof db>;
+export type DbTransactionAdapter = TransactionalAdapterDrizzleOrm<typeof db>;
 
 export default db;
