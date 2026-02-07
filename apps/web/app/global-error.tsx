c7092193315839ab872ccfa8f64bce042148862d
bibashmgr <bibashthapamagar21@gmail.com>
2026-02-07 22:11:35 +0545

    fix(web): wrap global-error page with theme-provider

--- apps/web/app/global-error.tsx
+++ apps/web/app/global-error.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { Courier_Prime, Open_Sans } from "next/font/google";
 
 import { Button } from "@repo/ui/components/button";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 import "@repo/ui/styles/globals.css";
 
@@ -39,22 +40,23 @@ export default function GlobalErrorPage({
   return (
     <html>
       <body className={`${fontSans.variable} ${fontMono.variable}`}>
-        <main className="w-full min-h-dvh flex flex-col justify-center items-center px-6 py-6">
-          <div className="max-w-xl flex flex-col justify-center items-center gap-6">
-            <div className="text-center">
-              <h2 className="text-3xl font-bold text-foreground">
-                Oops, something went wrong!
-              </h2>
-              <p className="text-lg text-muted-foreground">
-                We&apos;re sorry, but an unexpected error has occurred. Please
-                try again later or contact support if the issue persists.
-              </p>
+        <ThemeProvider>
+          <main className="w-full min-h-dvh flex flex-col justify-center items-center p-4">
+            <div className="max-w-sm flex flex-col justify-center items-center gap-6">
+              <div className="text-center space-y-2">
+                <h2 className="text-8xl font-bold text-foreground">500</h2>
+                <p className="text-base font-normal text-muted-foreground">
+                  We&apos;re sorry, but an unexpected error has occurred. Please
+                  try again later.
+                </p>
+              </div>
+
+              <Button variant="outline" onClick={handleReset}>
+                Try again
+              </Button>
             </div>
-            <Button variant="outline" onClick={handleReset}>
-              Try again
-            </Button>
-          </div>
-        </main>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
