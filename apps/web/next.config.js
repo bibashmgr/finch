194d2f981b14e6af75f52cb30724c150a96b6ac1
bibashmgr <bibashthapamagar21@gmail.com>
2026-02-12 16:03:01 +0545

    chore(web): set remote patterns in next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['http://localhost:8080'],
-  transpilePackages: ['@repo/ui'],
+  allowedDevOrigins: ["http://localhost:8080"],
+  transpilePackages: ["@repo/ui"],
+  images: {
+    minimumCacheTTL: 2678400,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dhq62j2x0/image/upload/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
