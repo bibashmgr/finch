eab1022a89d0e40b7b754ad3b4ff2298f0e00b09
bibashmgr <bibashthapamagar21@gmail.com>
2026-02-17 17:34:12 +0545

    feat: organize assets in cloudinary by folders

--- apps/api/src/modules/cloudinary/cloudinary.service.ts
+++ apps/api/src/modules/cloudinary/cloudinary.service.ts
@@ -8,12 +8,22 @@ import {
   CloudinaryDeleteResponse,
   CloudinaryUploadResponse,
 } from "@/modules/cloudinary/entities/cloudinary.type";
+import { ConfigService } from "@nestjs/config";
+import { Env } from "@/config/env.config";
 
 @Injectable()
 export class CloudinaryService {
+  constructor(private readonly configService: ConfigService<Env, true>) {}
+
   upload(file: Express.Multer.File): Promise<CloudinaryUploadResponse> {
     return new Promise<CloudinaryUploadResponse>((resolve, reject) => {
+      const folderName: string =
+        this.configService.get("APP_NAME").toLowerCase() +
+        "-" +
+        this.configService.get("NODE_ENV");
+
       const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: folderName },
         (error, result) => {
           if (error) return reject(error);
           resolve(result);
