4f85f5b029d0483ccfbd708942b3557bcd6b9c3b
bibashmgr <bibashthapamagar21@gmail.com>
2026-03-12 13:18:26 +0545

    feat(db): run inital migration for database

--- apps/api/migrations/0000_fancy_patriot.sql
+++ apps/api/migrations/0000_fancy_patriot.sql
@@ -1,13 +1,16 @@
 CREATE TYPE "public"."provider" AS ENUM('google', 'email');--> statement-breakpoint
 CREATE TYPE "public"."asset_type" AS ENUM('image', 'video', 'raw');--> statement-breakpoint
+CREATE TYPE "public"."currency" AS ENUM('usd', 'aud', 'npr');--> statement-breakpoint
+CREATE TYPE "public"."language" AS ENUM('en', 'ne');--> statement-breakpoint
+CREATE TYPE "public"."theme" AS ENUM('light', 'dark', 'system');--> statement-breakpoint
 CREATE TABLE "accounts" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid NOT NULL,
 	"provider" "provider" NOT NULL,
 	"provider_account_id" varchar(255) NOT NULL,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
-	CONSTRAINT "accounts_provider_provider_account_id_unique" UNIQUE("provider","provider_account_id")
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "accounts_provider_unique" UNIQUE("provider","provider_account_id")
 );
 --> statement-breakpoint
 CREATE TABLE "assets" (
@@ -23,8 +26,8 @@ CREATE TABLE "assets" (
 	"duration" varchar(20),
 	"url" text NOT NULL,
 	"deleted_at" timestamp,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "assets_public_id_unique" UNIQUE("public_id")
 );
 --> statement-breakpoint
@@ -36,9 +39,22 @@ CREATE TABLE "refresh_tokens" (
 	"ip_address" varchar(45),
 	"expires_at" timestamp NOT NULL,
 	"revoked_at" timestamp,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "refresh_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+CREATE TABLE "settings" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"currency" "currency" DEFAULT 'usd' NOT NULL,
+	"language" "language" DEFAULT 'en' NOT NULL,
+	"theme" "theme" DEFAULT 'system' NOT NULL,
+	"budget_alerts" boolean DEFAULT true NOT NULL,
+	"tips_articles_alerts" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "settings_user_id_unique" UNIQUE("user_id")
 );
 --> statement-breakpoint
 CREATE TABLE "users" (
@@ -46,8 +62,8 @@ CREATE TABLE "users" (
 	"email" varchar(255) NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"avatar_url" text,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
@@ -57,14 +73,16 @@ CREATE TABLE "verification_codes" (
 	"code_hash" varchar(255) NOT NULL,
 	"expires_at" timestamp NOT NULL,
 	"consumed_at" timestamp,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "assets" ADD CONSTRAINT "assets_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "settings" ADD CONSTRAINT "settings_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "assets_user_id_index" ON "assets" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX "refresh_token_lookup_index" ON "refresh_tokens" USING btree ("token");--> statement-breakpoint
+CREATE INDEX "settings_user_id_index" ON "settings" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX "user_email_index" ON "users" USING btree ("email");--> statement-breakpoint
 CREATE INDEX "verification_email_index" ON "verification_codes" USING btree ("email");
