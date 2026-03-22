02886b7b578f963ee95bf74f05458b4070c5bd31
bibashmgr <bibashthapamagar21@gmail.com>
2026-03-22 14:46:25 +0545

    fix: show empty view in notification page

--- apps/web/app/(protected)/notifications/_components/notification-list.tsx
+++ apps/web/app/(protected)/notifications/_components/notification-list.tsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { BellIcon } from "lucide-react";
 
-import { NotificationItem } from "@/components/notification-item";
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@repo/ui/components/empty";
 
 export function NotificationList() {
   return (
     <section className="flex flex-col">
-      {Array.from({ length: 5 }).map((_, index) => {
+      {/* {Array.from({ length: 5 }).map((_, index) => {
         return <NotificationItem key={index} />;
-      })}
+      })} */}
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <BellIcon />
+          </EmptyMedia>
+          <EmptyTitle>No notifications found</EmptyTitle>
+          <EmptyDescription>
+            Looks like there aren&apos;t any transactions to show right now.
+            Check back later.
+          </EmptyDescription>
+        </EmptyHeader>
+      </Empty>
     </section>
   );
 }
