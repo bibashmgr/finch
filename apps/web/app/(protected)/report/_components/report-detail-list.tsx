9d5d0a8fdfcd9381ea3af32d6db98379efbb8150
bibashmgr <bibashthapamagar21@gmail.com>
2026-03-26 23:06:29 +0545

    feat: add pagination and sorting in report page

--- apps/web/app/(protected)/report/_components/report-detail-list.tsx
+++ apps/web/app/(protected)/report/_components/report-detail-list.tsx
@@ -16,51 +16,105 @@ import { Skeleton } from "@repo/ui/components/skeleton";
 import { TransactionCard } from "@/components/transaction-card";
 
 import { useGetTransactionsQuery } from "@/store/apis/transaction-api";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@repo/ui/components/pagination";
+import { cn } from "@repo/ui/lib/utils";
+import { QueryOptions } from "./report-detail";
+import { ReportDetailSortDrawer } from "./report-detail-sort-drawer";
 
 type ReportDetailListProps = {
-  period: string;
+  queryOptions: QueryOptions;
+  handleChangeQueryOptions: (value: Partial<QueryOptions>) => void;
 };
 
-export function ReportDetailList({ period }: ReportDetailListProps) {
+export function ReportDetailList({
+  queryOptions,
+  handleChangeQueryOptions,
+}: ReportDetailListProps) {
+  const [isSortDrawerOpen, setIsSortDrawerOpen] =
+    React.useState<boolean>(false);
+
   const query = React.useMemo(() => {
+    const params = new URLSearchParams();
+
+    params.set("page", String(queryOptions.page));
+    params.set("limit", String(queryOptions.limit));
+
     const now = new Date();
 
-    if (period === "week") {
+    if (queryOptions.period === "week") {
       const since = new Date(now);
       since.setDate(since.getDate() - 6);
       since.setHours(0, 0, 0, 0);
 
-      const startDate = since.toISOString();
-      const endDate = now.toISOString();
-      return `startDate=${startDate}&endDate=${endDate}`;
+      params.set("startDate", since.toISOString());
+      params.set("endDate", now.toISOString());
     }
 
-    if (period === "month") {
+    if (queryOptions.period === "month") {
       const since = new Date(now);
       since.setMonth(since.getMonth() - 5);
       since.setDate(1);
       since.setHours(0, 0, 0, 0);
 
-      const startDate = since.toISOString();
-      const endDate = now.toISOString();
-      return `startDate=${startDate}&endDate=${endDate}`;
+      params.set("startDate", since.toISOString());
+      params.set("endDate", now.toISOString());
     }
 
-    if (period === "year") {
+    if (queryOptions.period === "year") {
       const since = new Date(now);
       since.setFullYear(since.getFullYear() - 4);
       since.setMonth(0, 1);
       since.setHours(0, 0, 0, 0);
 
-      const startDate = since.toISOString();
-      const endDate = now.toISOString();
-      return `startDate=${startDate}&endDate=${endDate}`;
+      params.set("startDate", since.toISOString());
+      params.set("endDate", now.toISOString());
     }
 
-    return "";
-  }, [period]);
+    if (queryOptions.sortBy === "newest") {
+      params.set("sortBy", "issuedAt:desc,createdAt:desc");
+    }
+
+    if (queryOptions.sortBy === "oldest") {
+      params.set("sortBy", "issuedAt:asc,createdAt:asc");
+    }
+
+    if (queryOptions.sortBy === "highest") {
+      params.set("sortBy", "amount:desc,createdAt:desc");
+    }
+
+    if (queryOptions.sortBy === "lowest") {
+      params.set("sortBy", "amount:asc,createdAt:desc");
+    }
+
+    return params.toString();
+  }, [queryOptions]);
 
   const { data, isLoading, isSuccess } = useGetTransactionsQuery(query);
+
+  const handlePagination = React.useCallback(
+    (action: "next" | "previous") => {
+      if (action === "next") {
+        handleChangeQueryOptions({
+          page: queryOptions.page + 1,
+        });
+      } else {
+        handleChangeQueryOptions({
+          page: queryOptions.page - 1,
+        });
+      }
+    },
+    [queryOptions],
+  );
+
+  const handleChangeSortDrawer = React.useCallback((value: boolean) => {
+    setIsSortDrawerOpen(value);
+  }, []);
 
   if (isLoading) {
     return (
@@ -74,7 +128,7 @@ export function ReportDetailList({ period }: ReportDetailListProps) {
         </div>
 
         <div className="space-y-3">
-          {Array.from({ length: 3 }).map((_, index) => {
+          {Array.from({ length: 5 }).map((_, index) => {
             return <Skeleton key={index} className="w-full h-18" />;
           })}
         </div>
@@ -141,7 +195,11 @@ export function ReportDetailList({ period }: ReportDetailListProps) {
       <div className="flex gap-2 justify-between items-center">
         <p className="text-base font-bold">Transactions</p>
 
-        <Button variant="secondary" size="icon">
+        <Button
+          variant="secondary"
+          size="icon"
+          onClick={() => handleChangeSortDrawer(true)}
+        >
           <ArrowUpDownIcon />
         </Button>
       </div>
@@ -159,6 +217,41 @@ export function ReportDetailList({ period }: ReportDetailListProps) {
           );
         })}
       </div>
+
+      {data.totalPages > 1 && (
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                aria-disabled={queryOptions.page === 1}
+                className={cn(
+                  "cursor-pointer",
+                  queryOptions.page === 1 && "pointer-events-none opacity-50",
+                )}
+                onClick={() => handlePagination("previous")}
+              />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext
+                aria-disabled={queryOptions.page === data.totalPages}
+                className={cn(
+                  "cursor-pointer",
+                  queryOptions.page === data.totalPages &&
+                    "pointer-events-none opacity-50",
+                )}
+                onClick={() => handlePagination("next")}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )}
+
+      <ReportDetailSortDrawer
+        queryOptions={queryOptions}
+        handleChangeQueryOptions={handleChangeQueryOptions}
+        isDrawerOpen={isSortDrawerOpen}
+        handleChangeDrawer={handleChangeSortDrawer}
+      />
     </div>
   );
 }
