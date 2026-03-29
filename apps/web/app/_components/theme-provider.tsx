bb3266b1ada1b15895f8780645240009b0329613
bibashmgr <bibashthapamagar21@gmail.com>
2026-03-29 17:34:20 +0545

    fix: resolve issue while changing theme

--- apps/web/app/_components/theme-provider.tsx
+++ apps/web/app/_components/theme-provider.tsx
@@ -10,7 +10,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme={setting?.theme ?? "system"}
+      defaultTheme={setting ? setting.theme : "system"}
       enableSystem
       disableTransitionOnChange
       enableColorScheme
